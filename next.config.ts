4016a61e9dcd5068517fdb855a551dcb33754835
Mouad.Bourbian <mouad.bourbian@edu.tbz.ch>
2026-02-07 23:36:22 +0000

    feat: enhance next.config.ts with GitHub Actions support for dynamic basePath

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from 'next';
 
+// This environment variable is automatically set by GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS || process.env.GITHUB_EVENT_NAME;
+
+let repoName = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
+}
+
+const basePath = isGithubActions && repoName ? `/${repoName}` : '';
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
+  basePath: basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
